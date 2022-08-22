46c560b296814d99cf0b0d1b7f3527b45df46e24
Pedro Longue <pedrolongue13@gmail.com>
2022-08-22 15:06:28 -0300

    cria toda a estruturação do projeto

---
+++ src/scripts/index.js
@@ -0,0 +1,45 @@
+async function searchCep() {
+  const cep = document.querySelector(".cep-input");
+  const cepValue = document.querySelector(".cep-input").value;
+  const invalidCep = document.querySelector(".error-message");
+  const validCep = cepValue.replace("-", "");
+
+  if (validCep.length !== 8) {
+    resetInput(cep);
+    invalidCep.innerHTML = `<p>Digite um cep válido!</p>`;
+    return;
+  } else {
+    invalidCep.innerHTML = ``;
+  }
+  let url = `https://viacep.com.br/ws/${cepValue}/json/`;
+  let response = await fetch(url);
+  let cepData = await response.json();
+
+  renderAdress(cepData, invalidCep);
+  resetInput(cep);
+}
+
+function renderAdress(cepData, invalidCep) {
+  let show = document.querySelector(".cep-informations");
+
+  if (cepData.erro) {
+    show.innerHTML = `<p>Endereço não localizado!</p>`;
+  } else {
+    show.innerHTML = `<div>
+                        <p>Cep: ${cepData.cep}</p>
+                        <p>Logradouro: ${cepData.logradouro}</p>
+                        <p>Complemento: ${cepData.complemento}</p>
+                        <p>Bairro: ${cepData.bairro}</p>
+                        <p>Localidade: ${cepData.localidade}</p>
+                        <p>UF: ${cepData.uf}</p>
+                        <p>Siafi: ${cepData.siafi}</p>
+                        <p>DDD: ${cepData.ddd}</p>
+                        <p>GIA: ${cepData.gia}</p>
+                        <p>IBGE: ${cepData.ibge}</p>
+                      </div>  `;
+  }
+}
+
+function resetInput(cep) {
+  cep.value = "";
+}
