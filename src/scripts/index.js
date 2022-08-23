c1e7f7b35fe5da33d9448eaade679c2d2add51c6
Pedro Longue <pedrolongue13@gmail.com>
2022-08-22 22:27:18 -0300

    Trata alguns erros

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,34 +1,36 @@
+const showCepInformations = document.querySelector(".cep-informations");
+
 async function searchCep() {
   const cep = document.querySelector(".cep-input");
   const cepValue = document.querySelector(".cep-input").value;
   const invalidCep = document.querySelector(".error-message");
-  const validCep = cepValue.replace("-", "");
+  const validCep = cepValue.replace("-", ""); //auxilia no tratamento dos erros
 
   if (validCep.length !== 8) {
+    //verifica se o cep não é válido
     resetInput(cep);
+    resetOutput();
     invalidCep.innerHTML = `<div>
                               <p>Digite um cep válido!</p>
                             </div>`;
-    return;
   } else {
     invalidCep.innerHTML = ``;
-    let url = `https://viacep.com.br/ws/${cepValue}/json/`;
+    let url = `https://viacep.com.br/ws/${cepValue}/json/`; //url da API
     let response = await fetch(url);
     let cepData = await response.json();
 
     renderAdress(cepData, invalidCep);
-    resetInput(cep, cepData);
+    resetInput(cep);
   }
 }
 
-function renderAdress(cepData, invalidCep) {
-  let show = document.querySelector(".cep-informations");
-
+function renderAdress(cepData) {
   if (cepData.erro) {
-    show.innerHTML = `<p>Endereço não localizado!</p>`;
+    showCepInformations.innerHTML = `<p>Endereço não localizado!</p>`; //Tratamento de erro, caso o cep informado não esteja na API
   } else {
-    show.innerHTML = `<div>
-                        <p>Cep: ${cepData.cep}</p>
+    //renderiza as informações
+    showCepInformations.innerHTML = `<div>
+                        <a href="https://www.google.com/search?q=${cepData.cep}&sxsrf=ALiCzsYr1Wu5qNOqUc7fcmpqMlVL7MbjrA%3A1661216707758&source=hp&ei=wycEY_C7K6zf1sQP7vC-gAw&iflsig=AJiK0e8AAAAAYwQ108JE2AEW7kNszZ6x35tTjNT-ynGp&ved=0ahUKEwiwqYi-4tv5AhWsr5UCHW64D8AQ4dUDCAY&uact=5&oq=28642000&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECc6BwgjEOoCECc6CwguEIAEEMcBEK8BOgUIABCABDoFCC4QgAQ6AggmOgUIABDLAVDMBFjLDGD8DWgBcAB4AIABjgGIAeQHkgEDMC44mAEAoAEBsAEK&sclient=gws-wiz">Cep: ${cepData.cep}</a>
                         <p>Logradouro: ${cepData.logradouro}</p>
                         <p>Complemento: ${cepData.complemento}</p>
                         <p>Bairro: ${cepData.bairro}</p>
@@ -44,4 +46,9 @@ function renderAdress(cepData, invalidCep) {
 
 function resetInput(cep) {
   cep.value = "";
+}
+
+function resetOutput() {
+  //reseta as informações renderizadas
+  showCepInformations.innerHTML = ``;
 }
